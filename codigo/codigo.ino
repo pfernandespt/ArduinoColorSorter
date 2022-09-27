ccf226c5fc1b7203c9e1b1c00019d071cb053e0b
Paulo Fernandes <paulofernandes@ua.pt>
2022-09-27 20:11:32 +0100

    versao 3 codigo

--- codigo/codigo.ino
+++ codigo/codigo.ino
@@ -1,3 +1,13 @@
+/* Este programa foi desenvolvido por Paulo Fernandes no ambito do projeto de fim de cadeira para IEE.
+ *  
+ *    Descrição: O seguinte codigo tem por objetivo através da utilização de um sensor de cor e de motores 
+ *    stepper executar a separação de coisas de até 6 cores diferentes tendo por base valores padrão indicados.
+ *    
+ *    Universidade: UAveiro | Departamento: DETI | Curso: EEC | Ano: 2022 | Cadeira: IEE | Professor: Pedro Cabral
+ *    Grupo nº8: Paulo Fernandes, Pedro Cunha
+ */
+
+
 //bibliotecas e variaveis para o sensor de cor //////////////////////////////////
 
 #include <Wire.h>
@@ -18,9 +28,17 @@ DFRobot_TCS34725 tcs = DFRobot_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_
 #include <AFMotor.h>
 #define StepsPerRevolution1 2048 //revoluções por volta motor 1 (cima)
 #define StepsPerRevolution2 200 //revoluções por volta motor 2 (baixo)
+#define Motor1Speed 20  //RPM maximo do motor 1
+#define Motor2Speed 400 //RPM maximo do motor 2
+#define Motor1Angle 90  // amplitude que o motor de cima tem que rodar para pegar mais um M&M
+#define Motor2Freedom 260 //amplitude maxima que o motor2 pode rodar
 
 AF_Stepper motor1(StepsPerRevolution1,1); //inicialização motor 1
 AF_Stepper motor2(StepsPerRevolution2,2); //inicialização motor 2
+
+int last_pos = 0, next_pos;
+const int rotation_step = map(Motor1Angle,0,360,0,StepsPerRevolution1); 
+const float  sector_amplitude = Motor2Freedom / 6;
 
 //bibliotecas e variaveis para os leds endereçaveis /////////////////////////////
 #include <FastLED.h>
@@ -31,28 +49,35 @@ AF_Stepper motor2(StepsPerRevolution2,2); //inicialização motor 2
 CRGB leds[NumLeds];
 
 //Processamento dos dados //////////////////////////////////////////////////////
-byte r,g,b, max_color, min_color; //processamento das cores para ficarem mais vivas
+byte r,g,b, max_color, min_color, nearest_color; //processamento das cores para ficarem mais vivas
 
 struct color{  
   byte r;
   byte g;
   byte b;
+  byte pos;
   char name[3];
 };
 
 const color cor[7] = { //cores padrao para a comparacao
   {255,255,255, "NOT"},
-  {255, 10, 10, "RED"}, //cor 1
-  {255,130,  8, "ORG"}, //cor 2
-  {255,243, 10, "YEL"}, //cor 3
-  { 10,255,170, "GRN"}, //cor 4
-  {  0,125,255, "BLU"}, //cor 5
-  {255, 20, 20, "BRW"}  //cor 6
+  {255, 10, 10, 4, "RED"}, //cor 1
+  {255,130,  8, 6, "ORG"}, //cor 2
+  {255,243, 10, 5, "YEL"}, //cor 3
+  { 10,255,170, 2, "GRN"}, //cor 4
+  {  0,125,255, 3, "BLU"}, //cor 5
+  {255, 20, 20, 1, "BRW"}  //cor 6
   };
+//Controlo /////////////////////////////////////////////////////////////////////
+#define Button A1
 
 void setup() { /////////////////////////////////////////////////////////////////
+  
   Serial.begin(9600);
-  Serial.println("Programa Inicializado!");
+  Serial.println("Comunicacao Serie Inicializada!");
+
+  motor1.setSpeed(Motor1Speed);
+  motor2.setSpeed(Motor2Speed);
 
   FastLED.addLeds<WS2812B, LedsPin, GRB>(leds, NumLeds); //inicialização da fita de leds
   FastLED.setBrightness(Brightness);
@@ -64,9 +89,21 @@ void setup() { /////////////////////////////////////////////////////////////////
     while(true); //se ocorrer um erro a encotrar o sensor o programa para a execução
   }
 
+  Serial.println("Tubo Seletor na Primeira posicao? Confirme...");
+  while(digitalRead(Button) == HIGH);
+  Serial.println("Tudo Certo! Iniciando Programa!");
+  while(digitalRead(Button) == LOW);
 }
 
 void loop() { /////////////////////////////////////////////////////////////////
+  //opcao de parar o programa 
+
+  if_pressed_stop();
+  
+  //ação do motor1
+  motor1.step(rotation_step, FORWARD, SINGLE);
+  
+  //ação do sensor de cor
   tcs.getRGBC(&sensor_red, &sensor_green, &sensor_blue, &sensor_clear); //leitura da cor do sensor
   tcs.lock();
   
@@ -83,7 +120,6 @@ void loop() { /////////////////////////////////////////////////////////////////
   Serial.print(") ->");
 
   // processamento dos valores para obter cores mais vivas
-  
   max_color = max(max(r,g),b);
   min_color = min(min(r,g),b);
 
@@ -95,6 +131,8 @@ void loop() { /////////////////////////////////////////////////////////////////
   g = map(g, 0, max_color, 0, 255);
   b = map(b, 0, max_color, 0, 255);
 
+  nearest_color = aprox_color();
+
   Serial.print(" ("); 
   Serial.print(r);
   Serial.print(';');
@@ -102,13 +140,19 @@ void loop() { /////////////////////////////////////////////////////////////////
   Serial.print(';');
   Serial.print(b);
   Serial.print(") Nearest color is ");
-  Serial.println(cor[aprox_color()].name);
+  Serial.println(cor[nearest_color].name);
+
+  //transmisao dos dados de cor para a fita de leds
 
   for(int i = 0; i < NumLeds; ++i){
     leds[i] = CRGB(r,g,b);
   }
   FastLED.show();
-  delay(1000);
+
+  //ação do motor2
+  align_motor2();
+  
+  delay(100);
 }
 //funções necessarias //////////////////////////////////////////////////////////////////////////
 
@@ -128,4 +172,23 @@ int aprox_color(){ //devolve o numero da cor mais proxima a que foi lida no sens
 float distance(int i){ // calcula a distancia da cor lida a cor indicada no argumento
   float dist = sqrt(pow((r-cor[i].r),2) + pow((g-cor[i].g),2) + pow((b-cor[i].b),2));
   return dist;
+}
+
+void align_motor2(){ //alinha o tubo com o local correto
+  int movement = abs(next_pos - last_pos);
+  movement *= sector_amplitude;
+  if(next_pos > last_pos) motor2.step(movement, FORWARD, DOUBLE);
+  else                    motor2.step(movement, BACKWARD, DOUBLE);
+  last_pos = next_pos;
+}
+
+void if_pressed_stop(){ //verifica se o botão esta pressionado e interrompe o funcionamento ate clicar novamente
+  if(digitalRead(Button) == LOW){
+    Serial.println("Programa em Pausa! Para retomar clique novamente no botao!");
+    while(digitalRead(Button) == LOW) delay(100);
+    
+    while(digitalRead(Button) == HIGH) delay(50);
+    Serial.println("Programa sera retomado! Por favor largue o botao.");
+    while(digitalRead(Button) == LOW) delay(100);
+  }
 }
