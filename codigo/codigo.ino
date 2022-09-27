e6ad27996a4fc3f7caa1c6ed5720c47dfac69101
Paulo Fernandes <paulofernandes@ua.pt>
2022-09-27 20:13:20 +0100

    versao 5 codigo

--- codigo/codigo.ino
+++ codigo/codigo.ino
@@ -5,6 +5,7 @@
  *    Universidade: UAveiro | Departamento: DETI | Curso: EEC | Ano: 2022 | Cadeira: IEE | Professor: Pedro Cabral
  *    Grupo nº8: Paulo Fernandes, Pedro Cunha
  */
+ 
 //bibliotecas e variaveis para o sensor de cor //////////////////////////////////
 
 #include <Wire.h>
@@ -25,22 +26,22 @@ DFRobot_TCS34725 tcs = DFRobot_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_
 #include <AFMotor.h>
 #define StepsPerRevolution1 200 //revoluções por volta motor 1 (cima)
 #define StepsPerRevolution2 200 //revoluções por volta motor 2 (baixo)
-#define Motor1Speed 50  //RPM maximo do motor 1
-#define Motor2Speed 50 //RPM maximo do motor 2
-#define Motor1Angle 90  // amplitude que o motor de cima tem que rodar para pegar mais um M&M
-#define Motor2Freedom 150 //amplitude maxima que o motor2 pode rodar
+#define Motor1Speed 50          //RPM maximo do motor 1
+#define Motor2Speed 50          //RPM maximo do motor 2
+#define Motor1Angle 45          //amplitude que o motor de cima tem que rodar para pegar mais um M&M
+#define Motor2Freedom 280       //amplitude maxima que o motor2 pode rodar
 
-AF_Stepper motor1(StepsPerRevolution1,2); //inicialização motor 1
-AF_Stepper motor2(StepsPerRevolution2,1); //inicialização motor 2
+AF_Stepper motor1(StepsPerRevolution1,1); //inicialização motor 1
+AF_Stepper motor2(StepsPerRevolution2,2); //inicialização motor 2
 
 int last_pos = 1, next_pos;
-//const int rotation_step = map(Motor1Angle,0,360,0,StepsPerRevolution1);
-const int rotation_step = 25;
-const float  sector_amplitude = Motor2Freedom / 6;
+const int rotation_step = map(Motor1Angle,0,360,0,StepsPerRevolution1);
+//const int rotation_step = 25;
+const float  sector_amplitude = map(Motor2Freedom, 0, 360, 0, StepsPerRevolution2)/ 6;
 
 //bibliotecas e variaveis para os leds endereçaveis /////////////////////////////
 #include <FastLED.h>
-#define NumLeds 20 //numero de leds da fita 
+#define NumLeds 60 //numero de leds da fita 
 #define LedsPin A0 //pino onde a fita esta ligada
 #define Brightness 255 //brilho dos leds 0 - 255
 
@@ -53,18 +54,18 @@ struct color{
   byte r;
   byte g;
   byte b;
-  byte pos;
-  char name[3];
+  byte pos; //posição no recipiente
+  char c;   //letra representativa
 };
 
 const color cor[7] = { //cores padrao para a comparacao
-  {255,255,255, 0, "NOT"},
-  {255, 10, 10, 1, "RED"}, //cor 1
-  {255,130,  8, 2, "ORG"}, //cor 2
-  {255,243, 10, 3, "YEL"}, //cor 3
-  { 10,255,170, 4, "GRN"}, //cor 4
-  {  0,125,255, 5, "BLU"}, //cor 5
-  {255, 20, 20, 6, "BRW"}  //cor 6
+  {255,255,255, 0, 'N'}, //nada detetado
+  {255, 10, 10, 1, 'R'}, //cor 1
+  {255,125, 10, 2, 'O'}, //cor 2
+  {255,243, 10, 3, 'Y'}, //cor 3
+  { 10,255,170, 4, 'G'}, //cor 4
+  {  0,125,255, 5, 'B'}, //cor 5
+  {255, 20, 20, 6, 'C'}  //cor 6
   };
 
 int contador[7];
@@ -74,13 +75,19 @@ int contador[7];
 
 void setup() { /////////////////////////////////////////////////////////////////
   pinMode(Button, INPUT_PULLUP);
+  
   Serial.begin(9600);
   Serial.println("Comunicacao Serie Inicializada!");
 
+  Serial.print("Motors:  M1: ");
+  Serial.print(sector_amplitude);
+  Serial.print("   M2: ");
+  Serial.println(rotation_step);
+  
   motor1.setSpeed(Motor1Speed);
   motor2.setSpeed(Motor2Speed);
 
-  for(int i = 0; i < 7; ++i) contador[i] = 0;
+  for(int i = 0; i < 7; ++i) contador[i] = 0; //inicializa o contador de cores a 0;
 
   FastLED.addLeds<WS2812B, LedsPin, GRB>(leds, NumLeds); //inicialização da fita de leds
   FastLED.setBrightness(Brightness);
@@ -99,12 +106,10 @@ void setup() { /////////////////////////////////////////////////////////////////
 }
 
 void loop() { /////////////////////////////////////////////////////////////////
-  //opcao de parar o programa 
-
-  if_pressed_stop();
+  
   delay(2000);
   //ação do motor1
-  motor1.step(rotation_step, FORWARD, MICROSTEP);
+  motor1.step(rotation_step, FORWARD, DOUBLE);
   
   //ação do sensor de cor
   tcs.getRGBC(&sensor_red, &sensor_green, &sensor_blue, &sensor_clear); //leitura da cor do sensor
@@ -134,8 +139,8 @@ void loop() { /////////////////////////////////////////////////////////////////
   g = map(g, 0, max_color, 0, 255);
   b = map(b, 0, max_color, 0, 255);
 
-  nearest_color = aprox_color();
-
+  nearest_color = aprox_color(); //chama a funcao
+  
   Serial.print(" ("); 
   Serial.print(r);
   Serial.print(';');
@@ -143,11 +148,11 @@ void loop() { /////////////////////////////////////////////////////////////////
   Serial.print(';');
   Serial.print(b);
   Serial.print(") Nearest color is ");
-  Serial.println(cor[nearest_color].name);
+  Serial.println(cor[nearest_color].c);
   contador[nearest_color]++;
   
-  for(int i = 0; i < 7; ++i){
-    Serial.print(cor[i].pos);
+  for(int i = 1; i < 7; ++i){
+    Serial.print(cor[i].c);
     Serial.print(": ");
     Serial.print(contador[i]);
     Serial.print("   ");
@@ -161,9 +166,48 @@ void loop() { /////////////////////////////////////////////////////////////////
   FastLED.show();
 
   //ação do motor2
+  delay(500);
   next_pos = cor[nearest_color].pos;
-  align_motor2();
+  int movement = abs(next_pos - last_pos);
+  movement *= sector_amplitude;
+  Serial.print("movimento motor baixo:");
+  Serial.println(movement);
+  if(next_pos > last_pos) {
+    motor2.step(movement, FORWARD, DOUBLE);
+    delay(100);
+  }
+  else {
+    motor2.step(movement, BACKWARD, DOUBLE);
+    delay(100);
+  }
   
+  last_pos = next_pos;
+
+  //opcao de parar o programa 
+  if(digitalRead(Button) == LOW){
+    Serial.println("Programa em Pausa! Para retomar clique novamente no botao!");
+    while(digitalRead(Button) == LOW) delay(100);
+
+    next_pos = 1;
+  int movement = abs(next_pos - last_pos);
+  movement *= sector_amplitude;
+  Serial.print("movimento motor baixo:");
+  Serial.println(movement);
+  if(next_pos > last_pos) {
+    motor2.step(movement, FORWARD, DOUBLE);
+    delay(100);
+  }
+  else {
+    motor2.step(movement, BACKWARD, DOUBLE);
+    delay(100);
+  }
+  
+  last_pos = next_pos;
+    
+    while(digitalRead(Button) == HIGH) delay(50);
+    Serial.println("Programa sera retomado! Por favor largue o botao.");
+    while(digitalRead(Button) == LOW) delay(100);
+  }
 }
 //funções necessarias //////////////////////////////////////////////////////////////////////////
 
@@ -183,34 +227,4 @@ int aprox_color(){ //devolve o numero da cor mais proxima a que foi lida no sens
 float distance(int i){ // calcula a distancia da cor lida a cor indicada no argumento
   float dist = sqrt(pow((r-cor[i].r),2) + pow((g-cor[i].g),2) + pow((b-cor[i].b),2));
   return dist;
-}
-
-void align_motor2(){ //alinha o tubo com o local correto
-  int movement = abs(next_pos - last_pos);
-  movement *= sector_amplitude;
-  Serial.print("movimento motor baixo:");
-  Serial.println(movement);
-  if(next_pos > last_pos) {
-    motor2.step(movement, FORWARD, MICROSTEP);
-    delay(100);
-  }
-  else {
-    Serial.print("MOV PARA TRAS");
-    Serial.println(movement);
-    motor2.step(movement, BACKWARD, MICROSTEP);
-    delay(100);
-  }
-  
-  last_pos = next_pos;
-}
-
-void if_pressed_stop(){ //verifica se o botão esta pressionado e interrompe o funcionamento ate clicar novamente
-  if(digitalRead(Button) == LOW){
-    Serial.println("Programa em Pausa! Para retomar clique novamente no botao!");
-    while(digitalRead(Button) == LOW) delay(100);
-    
-    while(digitalRead(Button) == HIGH) delay(50);
-    Serial.println("Programa sera retomado! Por favor largue o botao.");
-    while(digitalRead(Button) == LOW) delay(100);
-  }
 }
